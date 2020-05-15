d563b2678fcac1969367bd50c4992a4572ab4e7d
Zb <ztenerowicz@egnyte.com>
2020-05-15 21:40:54 +0200

    rewritten to fastify!

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const app = express();
+// @ts-check
 
-const data = {}
+const data = {};
 
 const styles = `
 <style>
@@ -25,75 +24,92 @@ small, small * {
     border: none;
 }
 </style>
-`
+`;
 
 const head = `
     <head>
         <meta charset="UTF-8">
+        <!-- hello from fastify -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         ${styles}
     </head>
-`
+`;
 
-function pickRandom(list){
-    return list[Math.floor(Math.random() * list.length)]
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
 }
 
-app.listen(3000, err => {
-    if (err) throw err
-    console.log(`> http://localhost:3000`)
+const fastify = require("fastify")({
+  logger: true,
 });
 
-app.use('/:game', (req, res, next) => {
-    req.game = req.params.game.replace(/[^0-9]+/g,'')
-    if (!data[req.game]) {
-        data[req.game] = []
-    }
-    next()
-})
-
-app.get('/', (req, res) => {
-    res.redirect(`/${Math.random().toFixed(5).substring(2)}`);
+fastify.get("/", async (request, reply) => {
+  reply.redirect(`/${Math.random().toFixed(5).substring(2)}`);
 });
-app.get('/:game/pick', (req, res) => {
-    const result = pickRandom(data[req.game])
-    res.send(`${head}<h1>${result}</h1><a href="/${req.game}?clear=y">clear</a>&nbsp;<a href="/${req.game}">back</a>`)
-})
-app.get('/trustmeimrandom', (req, res) => {
-    const list = ["A", "B", "C"];
-    const scores = { A: 0, B: 0, C: 0 };
-    
-    for (let i = 0; i < 10000000; i++) {
-        scores[pickRandom(list)]++
+
+fastify.decorateRequest("game", "");
+
+fastify.addHook("preHandler", async (request, reply) => {
+  if (request.params.game) {
+    const game = request.params.game.replace(/[^0-9]+/g, "");
+    if (!data[game]) {
+      data[game] = [];
     }
-    res.send(JSON.stringify(scores))
-})
-app.get('/:game', (req, res) => {
-    if (req.query.entry) {
-        data[req.game].push(req.query.entry);
-        return res.redirect(`/${req.game}`);
-    }
-    if (req.query.clear) {
-        data[req.game]=[]
-        return res.redirect(`/${req.game}`);
-    }
-    res.send(`
+    request.game = game;
+  }
+  return;
+});
+
+fastify.get("/:game", async (request, reply) => {
+  if (request.query.entry) {
+    data[request.game].push(request.query.entry);
+    return reply.redirect(`/${request.game}`); //FIXME
+  }
+  if (request.query.clear) {
+    data[request.game] = [];
+    return reply.redirect(`/${request.game}`);
+  }
+
+  reply.type("text/html").code(200);
+  return `
     ${head}
     <h2>Two of these people are lying.</h2>
-    <small>Game ${req.game}</small>
+    <small>Game ${request.game}</small>
     <form method="GET">
     <input type="text" name="entry" autofocus>
     &nbsp;
     <input type="submit" value="add">
     </form>
-    Added ${ data[req.game].length } articles
+    Added ${data[request.game].length} articles
     <p>
-    <a href="/${req.game}/pick">pick one</a>
-    <a onclick=" navigator.clipboard.writeText('${req.headers.host}/${req.game}')">copy game link</a>
+    <a href="/${request.game}/pick">pick one</a>
+    <a onclick=" navigator.clipboard.writeText('${request.headers.host}/${
+    request.game
+  }')">copy game link</a>
     </p>
     <br>
     <p><small><a href="https://www.youtube.com/watch?v=3yFEfOYTNoE">Learn the rules of the game</a></small></p>
-    `)
-
+    `;
 });
 
+fastify.get("/:game/pick", async (request, reply) => {
+  const result = pickRandom(data[request.game]);
+  reply.type("text/html").code(200);
+  return `${head}<h1>${result}</h1><a href="/${request.game}?clear=y">clear</a>&nbsp;<a href="/${request.game}">back</a>`;
+});
+
+fastify.get("/trustmeimrandom", (request, reply) => {
+  const list = ["A", "B", "C"];
+  const scores = { A: 0, B: 0, C: 0 };
+
+  for (let i = 0; i < 10000000; i++) {
+    scores[pickRandom(list)]++;
+  }
+  reply.type("application/json").code(200);
+  return scores;
+});
+
+fastify.listen(3000, (err, address) => {
+  if (err) throw err;
+  fastify.log.info(`> ${address}`);
+});
